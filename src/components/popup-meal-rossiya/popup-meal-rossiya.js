7e5e30ef9b3ed11882ee74b7bff2d98bcad72779
Alexey <a.petrik@mute-lab.com>
2018-02-02 16:17:03 +0400

    rossia meal popup

--- src/components/popup-meal-rossiya/popup-meal-rossiya.js
+++ src/components/popup-meal-rossiya/popup-meal-rossiya.js
@@ -30,13 +30,6 @@ function PopupMealRossiyaController(utils, $timeout, $scope) {
     vm.switchNext = switchNext;
     vm.switchPrev = switchPrev;
 
-    vm.availablePassengers = vm.passengers.filter(function (passenger, index) {
-        return (
-            vm.service.availableByPassengerSegments[index] &&
-            vm.service.availableByPassengerSegments[index][vm.currentFlightIndex]
-        );
-    });
-
     updateNextPrevState();
 	
 	$scope.$watch('vm.currentPassengerIndex', function () {
@@ -52,34 +45,64 @@ function PopupMealRossiyaController(utils, $timeout, $scope) {
     }
 
     function updateNextPrevState() {
-		for (var i=0; i<vm.availablePassengers.length; i++) {
+		for (var i=0; i<vm.passengers.length; i++) {
             if (i === vm.currentPassengerIndex) {
-                vm.nextPassenger = vm.availablePassengers[i + 1] || false;
-                vm.prevPassenger = vm.availablePassengers[i - 1] || false;
+				vm.nextPassenger = false;
+				vm.prevPassenger = false;
+				vm.nextPassengerIndex = false;
+				vm.prevPassengerIndex = false;
+				for (var j=i+1; j<vm.passengers.length; j++) {
+					if (
+						vm.service.availableByPassengerSegments[j] &&
+						vm.service.availableByPassengerSegments[j][vm.currentFlightIndex]
+					) {
+						vm.nextPassenger = vm.passengers[j];
+						vm.nextPassengerIndex = j;
+					}
+				}
+				for (var j=i-1; j>-1; j--) {
+					if (
+						vm.service.availableByPassengerSegments[j] &&
+						vm.service.availableByPassengerSegments[j][vm.currentFlightIndex]
+					) {
+						vm.prevPassenger = vm.passengers[j];
+						vm.prevPassengerIndex = j;
+					}
+				}
             }
         }
     }
 
     function switchNext() {
-        for (var i=0; i<vm.availablePassengers.length; i++) {
+        for (var i=0; i<vm.passengers.length; i++) {
             if (i === vm.currentPassengerIndex) {
-                if (vm.availablePassengers[i + 1]) {
-                    vm.selectFlightPassenger(vm.currentFlightIndex, i + 1);
-                    // Нужно дождаться применения нового currentPassengerIndex в родительском скопе
-                    $timeout(updateNextPrevState);
-                }
+                for (var j=i+1; j<vm.passengers.length; j++) {
+					if (
+						vm.service.availableByPassengerSegments[j] &&
+						vm.service.availableByPassengerSegments[j][vm.currentFlightIndex]
+					) {
+						vm.selectFlightPassenger(vm.currentFlightIndex, j);
+						// Нужно дождаться применения нового currentPassengerIndex в родительском скопе
+						$timeout(updateNextPrevState);
+					}
+				}
             }
         }
     }
 
     function switchPrev() {
-        for (var i=0; i<vm.availablePassengers.length; i++) {
+        for (var i=0; i<vm.passengers.length; i++) {
             if (i === vm.currentPassengerIndex) {
-				if (vm.availablePassengers[i - 1]) {
-                    vm.selectFlightPassenger(vm.currentFlightIndex, i - 1);
-                    // Нужно дождаться применения нового currentPassengerIndex в родительском скопе
-                    $timeout(updateNextPrevState);
-                }
+				for (var j=i-1; j>-1; j--) {
+					if (
+						vm.service.availableByPassengerSegments[j] &&
+						vm.service.availableByPassengerSegments[j][vm.currentFlightIndex]
+					) {
+						vm.selectFlightPassenger(vm.currentFlightIndex, j);
+						// Нужно дождаться применения нового currentPassengerIndex в родительском скопе
+						$timeout(updateNextPrevState);
+					}
+				}
             }
         }
     }
