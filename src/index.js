9782c91a42111eb7582eafb68374e94e48b627f9
Anton-Malyugin <a.malyugin@mute-lab.com>
2018-03-16 13:45:50 +0400

    edit Valentin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-
-
-
-console.log('app: ', angular.module('app'));
-
 require('./components/popup-meal-rossiya/popup-meal-rossiya');
 require('./components/es-meal-rossiya/es-meal-rossiya');
 require('./components/es-seat-rossiya/es-seat-rossiya');
